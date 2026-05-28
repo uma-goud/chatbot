059991060049b885c5d6cf1f490b42901e05bf85
uma <ubaleboyina@gmail.com>
2026-05-28 16:50:03 +0530

    Added chatbot updates

--- chatbot.py
+++ chatbot.py
@@ -4,14 +4,17 @@ import google.generativeai as genai
 
 load_dotenv()
 
-api_key=os.getenv("GEMINI_API_KEY")
-
+api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("models/gemini-2.5-flash")
+model = genai.GenerativeModel(
+    "models/gemini-2.5-flash",
+    system_instruction="Always reply in simple text. Do NOT use LaTeX or $ symbols."
+)
 
 print("Chatbot started! Type 'exit' to stop")
-chat_history=[]
+
+chat_history = []
 
 while True:
     user_input = input("You: ")
@@ -19,13 +22,14 @@ while True:
     if user_input.lower() == "exit":
         print("Chat ended.")
         break
-    chat_history.append(f"you:{user_input}")
-    full_prompt="\n".join(chat_history)
 
-    response = model.generate_content(user_input)
+    chat_history.append(f"You: {user_input}")
+    full_prompt = "\n".join(chat_history)
+
     try:
-        response=model.generate_content(full_prompt)
-        chat_history.append(f"AI:{response.text}")
+        response = model.generate_content(full_prompt)
+        chat_history.append(f"AI: {response.text}")
         print("AI:", response.text)
+
     except Exception as e:
         print("Error:", e)
